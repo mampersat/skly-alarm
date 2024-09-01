d3f1efcce5e5c44d6549e8c5c520eaef96ea82bc
Matt Sheppard <msheppard@beta.team>
2024-09-01 16:57:00 -0400

    time / location / cleanup

--- README.md
+++ README.md
@@ -5,9 +5,23 @@ Create a device as described in https://xkcd.com/2979/
 ![sky alarm](https://imgs.xkcd.com/comics/sky_alarm.png)
 
 ## Data Sources
-* https://api.auroras.live
-* https://www.spaceweather.gov
-* https://api.nasa.gov
+* https://ipinfo.io
+* https://worldtimeapi.org/api/timezone/{tzid}
+* http://auroraslive.io/#/api/v1/all
+* https://api.auroras.live/v1/?type=all&lat={lat}&long={lon}&forecast=false&threeday=false
+* http://api.open-notify.org/iss-now.json
+* https://api.sunrise-sunset.org/json?lat={lat}&lng={lon}&date=today&tzid={tzid}
+* https://api.nasa.gov/neo/rest/v1/feed?start_date=2018-10-10&end_date=2018-10-10&api_key=DEMO
+* https://launchlibrary.net/1.4/launch/next/5
+* https://api.nasa.gov/planetary/apod
+* https://opensky-network.org/api/states/all
+
 
 ## Hardware
 * [Raspberry PI picoPI](https://www.raspberrypi.com/products/raspberry-pi-pico/)
+* [Neopixels](https://www.adafruit.com/product/2541)
+* Case : Work in Progress
+
+## Definition of Done
+
+Working prototype delivered to XKCD
--- main.py
+++ main.py
@@ -3,6 +3,25 @@ import requests
 # lat/lon of KBTV
 lat = 44.4689
 lon = -73.1502
+tzid = "America/New_York"
+
+def get_location():
+    # Get location from IP
+    global lat, lon, tzid
+
+    response = requests.get("https://ipinfo.io")
+    location = response.json()
+    lat = float(location['loc'].split(',')[0])
+    lon = float(location['loc'].split(',')[1])
+    tzid = location['timezone']
+    print(f"{location['city']}, {location['region']} : {location['country']}")
+
+    # get current time
+    response = requests.get(f"https://worldtimeapi.org/api/timezone/{tzid}")
+    time = response.json()    
+    print(f"time: {time['datetime']}")
+            
+    return True
 
 def aurora():
     # Check for northen lights
@@ -28,21 +47,57 @@ def iss():
     # check if ISS is overhead
     range = 10
     if iss_lat - range < lat < iss_lat + range and iss_lon - range < lon < iss_lon + range:
+        print('ISS is overhead')
         return True
+    
+    print('ISS is not overhead')
+
 
 def sun():
     # Check for sunrise and sunset
     # Should cache this to 24hrs
-    # More interesting if we can check atmospheric conditions like haze and cloud cover
-    response = requests.get("https://api.sunrise-sunset.org/json?lat=65.0&lng=25.0&date=today")
+    # More interesting if we can check atmospheric conditions like haze and cloud cover, aka "Pretty Sunsets"
+    request = f"https://api.sunrise-sunset.org/json?lat={lat}&lng={lon}&date=today&tzid={tzid}"
+    print(request)
+
+    response = requests.get(request)
+    sunrise = response.json()['results']['sunrise']
+    sunset = response.json()['results']['sunset']
+
+    print(f"Sunrise: {sunrise}")
+    print(f"Sunset: {sunset}")
 
 def meteors():
     # Check for meteors
     response = requests.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=2018-10-10&end_date=2018-10-10&api_key=DEMO")
 
+
 def launches():
     # Check for rocket launches
     response = requests.get("https://launchlibrary.net/1.4/launch/next/5")
 
-aurora()
-iss()
+
+def neatclouds():
+    # Check for neat cloud formations
+    response = requests.get("https://api.nasa.gov/planetary/apod")
+
+
+def airplanes():
+    # Check for airplanes
+    response = requests.get("https://opensky-network.org/api/states/all")
+
+
+def main():
+    print('Checking for cool things in the sky')
+    get_location()
+    aurora()
+    iss()
+    sun()
+    # meteors()
+    # launches()
+    # neatclouds()
+    # airplanes()
+
+if __name__ == '__main__':
+    main()
+    
